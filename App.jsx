286d5a72d3d41c5cf48ea421d400ca5f31ce37fc
Anshul Prasad <71313839+A2508l@users.noreply.github.com>
2021-06-06 01:14:45 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import "./styles.css";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React Bootcamp</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde velit ut
+        consequuntur incidunt quidem vero amet quasi iure sit eligendi.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
